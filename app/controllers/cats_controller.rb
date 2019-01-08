742a4245e192d5284cef95c0873737273cc5d556
smithwebtek <brad@smithwebtek.com>
2019-01-07 23:46:06 -0500

    Set up dog and cat CRUD

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,45 @@
 class CatsController < ApplicationController
+before_action :set_cat, only: [:show, :edit, :update, :destroy]
+	
+	def index
+		@cats = Cat.all
+	end
+
+	def show
+	end
+
+	def new
+		@cat = Cat.new
+	end
+
+	def create
+		@cat = Cat.new(cat_params)
+		if @cat.save
+			respond_to do |f|
+				f.html { redirect_to cat_path(@cat)}
+				f.json { render json: @cat}
+			end
+		else
+			render :new
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		@cat = Cat.find(params[:id])
+	end
+
+	def destroy
+	end
+
+	private
+		def set_cat
+			@cat = Cat.find(params[:id])
+		end
+
+		def cat_params
+			params.require(:cat).permit(:owner_id, :name, :breed)
+		end
 end
--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,45 @@
 class OwnersController < ApplicationController
-end
+	before_action :set_owner, only: [:show, :edit, :update, :destroy]
+		
+		def index
+			@owners = Owner.all
+		end
+	
+		def show
+		end
+	
+		def new
+			@owner = Owner.new
+		end
+	
+		def create
+			@owner = Owner.new(owner_params)
+			if @owner.save
+				respond_to do |f|
+					f.html { redirect_to owner_path(@owner)}
+					f.json { render json: @owner}
+				end
+			else
+				render :new
+			end
+		end
+	
+		def edit
+		end
+	
+		def update
+			@owner = Owner.find(params[:id])
+		end
+	
+		def destroy
+		end
+	
+		private
+			def set_owner
+				@owner = Owner.find(params[:id])
+			end
+	
+			def owner_params
+				params.require(:owner).permit(:name, :city)
+			end
+	end
